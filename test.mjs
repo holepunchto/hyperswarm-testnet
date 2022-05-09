2a6180a125f0c9c092016fe45a0254fc89f39cc7
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-05-09 14:41:33 +0200

    Add slow test case

--- test.mjs
+++ test.mjs
@@ -7,3 +7,17 @@ test('basic', async (t) => {
   t.is(testnet.nodes.length, 3, 'has 3 nodes')
   t.alike(testnet.nodes, [...testnet], 'can iterate nodes')
 })
+
+test('createNode + createServer', async (t) => {
+  t.comment('should not cause request timeouts')
+
+  const testnet = await createTestnet(3, t.teardown)
+
+  const a = testnet.createNode()
+  await a.ready()
+
+  const server = a.createServer()
+  await server.listen()
+
+  t.pass()
+})
